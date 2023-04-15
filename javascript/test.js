838d63c1b6a9463e0584849e025bec0406d07262
sencye <yechunsen@gmail.com>
2023-04-15 16:23:05 +0800

    feat: change ui

--- javascript/test.js
+++ javascript/test.js
@@ -1,13 +1,20 @@
 // 页面更新时控制其他数据的显示
 const BaseUrl = 'http://v118-27-14-73.9ob0.static.cnode.io'
-
-onUiUpdate(function () {
+window.roomId = 'window.roomId'
+window.lastImgSrc = ''
+onUiUpdate(async function () {
   try {
     const progress = gradioApp().querySelector('#txt2img_results > .progressDiv > .progress');
     const image = gradioApp().querySelector('#txt2img_gallery > .grid-wrap > .grid-container > button > img');
     const showImage = gradioApp().querySelector('#showImage');
     if (image) {
-      showImage.src = image.src
+      if (image.src !== window.lastImgSrc) {
+        console.log('image.src', image.src);
+        showImage.src = image.src
+        window.lastImgSrc = image.src
+        const res = await upLoadImage(image.src)
+        console.log('res', res);
+      }
     }
     const progressContainer = gradioApp().querySelector('.progress-container');
     const showProgress = gradioApp().querySelector('.active-line-number > input');
@@ -30,12 +37,12 @@ onUiUpdate(function () {
 });
 
 onUiLoaded(async function () {
-  await getUserIp()
-  window.getRoomInfo = getRoomInfo
   const userIp = gradioApp().querySelector('#user-ip');
   userIp.innerHTML = window.ip || ''
   const promptText = gradioApp().querySelector('#prompt-text');
   const generateBtn = gradioApp().querySelector('#generate-btn');
+  const sendBtn = gradioApp().querySelector('#send-message-button');
+  console.log('sendBtn', sendBtn);
   const cancelBtn = gradioApp().querySelector('#cancel-btn');
   const t2iGenerateBtn = gradioApp().querySelector('#txt2img_generate');
   const t2iSkipBtn = gradioApp().querySelector('#txt2img_skip');
@@ -49,6 +56,13 @@ onUiLoaded(async function () {
   if (!generateBtn || !t2iGenerateBtn) {
     return
   }
+  sendBtn.addEventListener('click', async () => {
+    console.log('sendBtn');
+    const sendMessageInput = gradioApp().querySelector('#send-message');
+    const res = await sendMessage(sendMessageInput.value)
+    console.log('res', res);
+    getChatHistory()
+  })
   generateBtn.addEventListener('click', () => {
     t2iGenerateBtn.click()
   })
@@ -58,6 +72,13 @@ onUiLoaded(async function () {
   cancelBtn.addEventListener('click', () => {
     t2iSkipBtn.click()
   })
+  await getUserIp()
+  await getChatHistory()
+  await getImage()
+  window.getRoomInfo = getRoomInfo
+  window.getChatHistory = getChatHistory
+  window.sendMessage = sendMessage
+  window.getImage = getImage
 })
 
 
@@ -70,21 +91,60 @@ const getUserIp = async () => {
 
 const getRoomInfo = async () => {
   const res = await fetch(BaseUrl + '/room/info?' + new URLSearchParams({
-    room_id: '99999aa',
+    room_id: 'window.roomId',
     ip_address: window.ip
   }))
   const data = await res.json()
   console.log('data', data);
 }
+
 const createRoom = async () => {
   const res = await fetch('http://v118-27-14-73.9ob0.static.cnode.io/')
   const data = await res.json()
   const { ip = '' } = data
   window.ip = ip
 }
-const upLoadImage = async () => {
-  const res = await fetch('http://v118-27-14-73.9ob0.static.cnode.io/')
+
+const sendMessage = async (message) => {
+  const data = {
+    room_id: window.roomId,
+    user_id: 0,
+    message,
+  }
+  const res = await fetch(BaseUrl + '/chat/send', {
+    body: JSON.stringify(data),
+    method: "POST",
+  })
+  const json = await res.json()
+  console.log('sendMessage', json);
+}
+const upLoadImage = async (url) => {
+  const promptText = gradioApp().querySelector('#prompt-text');
+  const data = {
+    "room_id": window.roomId,
+    "prompt": promptText.value,
+    "image_url": url
+  }
+  const res = await fetch(BaseUrl + '/image/upload', {
+    body: JSON.stringify(data),
+    method: "POST",
+  })
+  const json = await res.json()
+  console.log('sendMessage', json);
+}
+
+const getImage = async (url) => {
+  const res = await fetch(BaseUrl + '/image/get?' + new URLSearchParams({
+    room_id: window.roomId,
+  }))
+  const json = await res.json()
+  console.log('sendMessage', json);
+}
+
+const getChatHistory = async () => {
+  const res = await fetch(BaseUrl + '/chat/history?' + new URLSearchParams({
+    room_id: window.roomId,
+  }))
   const data = await res.json()
-  const { ip = '' } = data
-  window.ip = ip
+  console.log('getChatHistory', data);
 }
