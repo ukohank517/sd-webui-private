3921396b1f05a9f4714af3505f480f55c750635c
sencye <yechunsen@gmail.com>
2023-04-22 14:03:01 +0800

    feat: Add multi language

--- javascript/test.js
+++ javascript/test.js
@@ -1,12 +1,3 @@
-const NameMap = {
-  0: '房主',
-  1: '用户1',
-  2: '用户2',
-  3: '用户3',
-  4: '用户4',
-  5: '用户5',
-  6: '用户6',
-}
 const AvatarMap = {
   0: "./file=html/avatar0.svg",
   1: "./file=html/avatar1.svg",
@@ -17,12 +8,74 @@ const AvatarMap = {
   6: "./file=html/avatar6.svg",
 }
 
+let langMenu = 'hide'
 // 页面更新时控制其他数据的显示
 const BaseUrl = 'http://v118-27-14-73.9ob0.static.cnode.io'
 window.roomId = window.location.search.replaceAll('?room_id=', '') || ''
 console.log('roomId', window.roomId);
 window.lastImgSrc = ''
 window.chatHistoryLength = 0
+
+const TextMap = {
+  'en': {
+    title: 'Promptcoin - Artificial Intelligence and Human Intelligence Collaboration Platform',
+    modelTip: 'Select model here',
+    ower: 'room owner',
+    user: 'user',
+    chatroom: 'Chat room',
+    generate: 'generate',
+    send: 'send',
+    sendmessage: 'Send a message',
+    infoTip: 'INTRODUCING ONIX',
+    desc: 'Offering you and your community AI tools with social features, collaboratively creating personalized content,and rewarding participants with tokens. Soon to be launched model library: NFT creation model, market forecasting model, media sentiment analysis model, KOL content generation model, and more.'
+  },
+  'jp': {
+    title: 'Promptcoin - 人工知能と人間智能の協業プラットフォーム',
+    modelTip: 'ここでモデルを選択してください',
+    ower: '部屋のオーナー',
+    user: 'ユーザー',
+    chatroom: 'チャットルーム',
+    generate: '生成する',
+    send: '送信する',
+    sendmessage: 'メッセージを送信する',
+    infoTip: 'ONIXを紹介します',
+    desc: 'AIツールとソーシャル機能を備えたコミュニティ向けのONIXを提供し、参加者にトークンで報酬を与えながら、共同でパーソナライズされたコンテンツを作成することができます。近日中にモデルライブラリーを発表予定です。NFT作成モデル、市場予測モデル、メディア感情分析モデル、KOLコンテンツ生成モデルなどがあります。'
+  },
+  'zh': {
+    title: 'Promptcoin - 人工智能與人類智能協作平台',
+    modelTip: '在這裡選擇模型',
+    ower: '房主',
+    user: '用户',
+    chatroom: '聊天室',
+    generate: '生成',
+    send: '傳送',
+    sendmessage: '傳送訊息',
+    infoTip: '介紹ONIX',
+    desc: '為您和您的社群提供具有社交功能的AI工具，合作創建個性化內容，並通過代幣獎勵參與者。即將推出的模型庫包括：NFT創建模型，市場預測模型，媒體情感分析模型，KOL內容生成模型等。'
+  },
+}
+const changeLang = (lang = 'en') => {
+  const promptcoinTitle = gradioApp().querySelector('#promptcoin-title');
+  const generateBtn = gradioApp().querySelector('#generate-btn');
+  const sendButton = gradioApp().querySelector('#send-message-button');
+  const modelTip = gradioApp().querySelector('#setting_sd_model_checkpoint > label > .svelte-1gfkn6j');
+  const infoTip = gradioApp().querySelector('.wrap-info .left-box .tips');
+  const infoTitle = gradioApp().querySelector('.wrap-info .left-box #title');
+  const desc = gradioApp().querySelector('.wrap-info .left-box .desc');
+  const chatroom = gradioApp().querySelector('.chat-room-header > h2 ');
+  const sendMessage = gradioApp().querySelector('.send-message-header > span ');
+  promptcoinTitle.innerHTML = TextMap[lang].title
+  modelTip.innerHTML = TextMap[lang].modelTip
+  chatroom.innerHTML = TextMap[lang].chatroom
+  generateBtn.innerHTML = TextMap[lang].generate
+  sendButton.innerHTML = TextMap[lang].send
+  sendMessage.innerHTML = TextMap[lang].sendmessage
+  infoTitle.innerHTML = TextMap[lang].title
+  infoTip.innerHTML = TextMap[lang].infoTip
+  desc.innerHTML = TextMap[lang].desc
+  initChatHistory()
+}
+
 onUiUpdate(async function () {
   try {
     const progress = gradioApp().querySelector('#txt2img_results > .progressDiv > .progress');
@@ -68,7 +121,13 @@ onUiLoaded(async function () {
   const cancelBtn = gradioApp().querySelector('#cancel-btn');
   const t2iGenerateBtn = gradioApp().querySelector('#txt2img_generate');
   const t2iSkipBtn = gradioApp().querySelector('#txt2img_skip');
-
+  const langIcon = gradioApp().querySelector('#lang-icon');
+  const langMenuDom = gradioApp().querySelector('#lang-menu');
+  const setEnDom = gradioApp().querySelector('#set-en');
+  const setJpDom = gradioApp().querySelector('#set-jp');
+  const setZhDom = gradioApp().querySelector('#set-zh');
+  const langStore = localStorage.getItem('lang')
+  changeLang(langStore)
   // const chatRoomHistory = gradioApp().querySelector('.chat-room-history');
   // const leftBox = gradioApp().querySelector('.left-box');
   // const boundingClient = leftBox.getBoundingClientRect()
@@ -98,6 +157,49 @@ onUiLoaded(async function () {
   sendBtn.addEventListener('click', async () => {
     await sendMessage()
     initChatHistory()
+  })
+  langIcon.addEventListener('click', async () => {
+    const lang = localStorage.getItem('lang') || 'en'
+    if (lang === 'en') {
+      setEnDom.style.border = '1px solid #fff';
+    } else {
+      setEnDom.style.border = 'none'
+    }
+    if (lang === 'jp') {
+      setJpDom.style.border = '1px solid #fff';
+    } else {
+      setJpDom.style.border = 'none'
+    }
+    if (lang === 'zh') {
+      setZhDom.style.border = '1px solid #fff';
+    } else {
+      setZhDom.style.border = 'none'
+    }
+    if (langMenu === 'show') {
+      langMenu = 'hide'
+      langMenuDom.style.opacity = 0
+    } else {
+      langMenu = 'show'
+      langMenuDom.style.opacity = 1
+    }
+  })
+  setEnDom.addEventListener('click', () => {
+    langMenuDom.style.opacity = 0
+    langMenu = 'hide'
+    localStorage.setItem('lang', 'en')
+    window.location.reload()
+  })
+  setJpDom.addEventListener('click', () => {
+    langMenuDom.style.opacity = 0
+    langMenu = 'hide'
+    localStorage.setItem('lang', 'jp')
+    window.location.reload()
+  })
+  setZhDom.addEventListener('click', () => {
+    langMenuDom.style.opacity = 0
+    langMenu = 'hide'
+    localStorage.setItem('lang', 'zh')
+    window.location.reload()
   })
   generateBtn.addEventListener('click', () => {
     if (window.userId === 0) {
@@ -284,40 +386,14 @@ const getMemberNumber = async (url) => {
   }
 }
 
-const updateChatHistory = async () => {
-  try {
-    const res = await fetch(BaseUrl + '/chat/history?' + new URLSearchParams({
-      room_id: window.roomId,
-    }))
-    const data = await res.json()
-    const { chat_history } = data
-    if (!chat_history.length) {
-      return
-    }
-    if (window.chatHistoryLength === chat_history.length) {
-      return
-    }
-    const chatRoomHistory = gradioApp().querySelector('.chat-room-history');
-    const messageExample = gradioApp().querySelector('#message-example');
-    chatRoomHistory.innerHTML = ''
-    const i = chat_history.length - 1
-    const child = messageExample.cloneNode(true)
-    child.style.display = 'flex'
-    child.setAttribute('id', `message-item-${i}`)
-    await chatRoomHistory.appendChild(child)
-    const avatar = child.querySelector(`.avatar-box > img`);
-    avatar.src = AvatarMap[chat_history[i].user_id]
-    const messageName = child.querySelector(`.message-box > p > .name`);
-    const messageTime = child.querySelector(`.message-box > p > .time`);
-    const messageText = child.querySelector(`.message-box > .message-text`);
-    messageName.innerHTML = NameMap[chat_history[i].user_id]
-    messageText.innerHTML = chat_history[i].message
-    messageTime.innerHTML = chat_history[i].created_at
-    window.chatHistoryLength = chat_history.length
-  } catch (error) {
-    console.log('updateChatHistory', error);
+const getUserName = (id) => {
+  const lang = localStorage.getItem('lang')
+  if (id === 0) {
+    return TextMap[lang].ower
   }
+  return `${TextMap[lang].user} ${id}`
 }
+
 
 const initChatHistory = async () => {
   try {
@@ -345,7 +421,7 @@ const initChatHistory = async () => {
       const messageName = child.querySelector(`.message-box > p > .name`);
       const messageTime = child.querySelector(`.message-box > p > .time`);
       const messageText = child.querySelector(`.message-box > .message-text`);
-      messageName.innerHTML = NameMap[chat_history[i].user_id]
+      messageName.innerHTML = getUserName(chat_history[i].user_id)
       messageText.innerHTML = chat_history[i].message
       messageTime.innerHTML = chat_history[i].created_at
     }
